4ee4c413632f90f066819a8d1cd26cc1f9732c51
RadonZ <supachai27102000@gmail.com>
2024-02-23 21:11:27 +0700

    use Printf instead of Println

--- 02-go-essentials/first-app/investment-calculator.go
+++ 02-go-essentials/first-app/investment-calculator.go
@@ -5,24 +5,26 @@ import (
 	"math"
 )
 
-func main(){
+func main() {
 	const inflationRate = 6.5
 	var investmentAmount float64
-	var years  float64
+	var years float64
 	var expectedReturnRate float64
 
 	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
-	
+
 	fmt.Print("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
 	fmt.Print("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue) 
-	fmt.Println(futureRealValue) 
+	// output information
+	// fmt.Println("Future Value: ",futureValue)
+	fmt.Printf("Future Value: %v\nFuture Value (adjusted for Information): %v", futureValue, futureRealValue)
+	// fmt.Println("Future Value (adjusted for Inflation): ",futureRealValue)
 }
