e8f8b6557fab29af7fdb4e386721411b82a61427
RadonZ <supachai27102000@gmail.com>
2024-02-24 23:42:39 +0700

    using function version

--- 02-go-essentials/profit-calculator-exercise/profit_calculator.go
+++ 02-go-essentials/profit-calculator-exercise/profit_calculator.go
@@ -5,28 +5,34 @@ import (
 )
 
 func main() {
-	var revenue float64
-	var expense float64
-	var tax_rate float64
 
-	fmt.Print("your revenue is ")
-	fmt.Scan(&revenue)
 
-	fmt.Print("your expense is ")
-	fmt.Scan(&expense)
+	revenue := getUserInput("your revenue is ")
 
-	fmt.Print("Tax Rate is ")
-	fmt.Scan(&tax_rate)
+	expense := getUserInput("your expense is ")
 
-	
+	tax_rate := getUserInput("Tax Rate is ")
 
-	
+	EBTValue, ProfitValue, RatioValue := calculatorValue(revenue, expense, tax_rate)
+
+	formattedEBT := fmt.Sprintf("Earned before Tax Value: %.2f\n", EBTValue)
+	formattedProfit := fmt.Sprintf("Earned after Tax Value: %.2f\n", ProfitValue)
+	formattedRatio := fmt.Sprintf("Ratio: %.2f\n", RatioValue)
+
+	fmt.Print(formattedEBT, formattedProfit, formattedRatio)
 
 }
 
-func calculatorValue(revenue,expense,tax_rate float64) (ebt,profit,ratio float64) {
+func getUserInput(text string) float64 {
+	var userInput float64
+	fmt.Print(text)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculatorValue(revenue, expense, tax_rate float64) (ebt, profit, ratio float64) {
 	ebt = revenue - expense
 	profit = float64(ebt) * (1 - tax_rate/100)
 	ratio = ebt / profit
-	return ebt,profit,ratio
+	return ebt, profit, ratio
 }
