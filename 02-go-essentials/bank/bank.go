a2ff17496b8a3f3f5198fdd28213e3bb771188c8
RadonZ <supachai27102000@gmail.com>
2024-02-24 23:43:00 +0700

    condition part

---
+++ 02-go-essentials/bank/bank.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func main() {
+	var accountBalance = 1000.00
+
+	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	// wantCheckBalance := choice == 1
+
+	if choice == 1 {
+		fmt.Println("Your balance is", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+
+		if depositAmount <= 0 {
+			fmt.Println("Invalia amount. Must be greater than 0.")
+			return
+		}
+
+		accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+		fmt.Println("Balance updated! New amount: ", accountBalance)
+	} else if choice == 3 {
+		fmt.Print("Your withdraw: ")
+		var withdrawalAmount float64
+		fmt.Scan(&withdrawalAmount)
+
+		if withdrawalAmount <= 0 {
+			fmt.Println("Invalia amount. Must be greater than 0.")
+			return
+		}
+		if withdrawalAmount > accountBalance {
+			fmt.Println("Invalia amount. You can't withdraw more than you have.")
+			return
+		}
+		accountBalance -= withdrawalAmount // accountBalance = accountBalance + withdrawalAmount
+		fmt.Println("Balance updated! New amount: ", accountBalance)
+	} else {
+		fmt.Println("Thank you for using the service.")
+	}
+
+	fmt.Println("Your choice: ", choice)
+}
