cbb5f33c5d12922b99a1cd42b26682d1022da1bb
RadonZ <supachai27102000@gmail.com>
2024-02-26 20:53:50 +0700

    exercise error handle and write file

--- 02-go-essentials/profit-calculator-exercise/profit_calculator.go
+++ 02-go-essentials/profit-calculator-exercise/profit_calculator.go
@@ -1,17 +1,67 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
+
+// Goals
+// 1) Validate user input
+// 	=> Show error message & exit if invalid input is provided
+// 	- No negative numbers
+// 	- Not 0
+// 2) Store calculated results into file
+
+const resultsFile = "result.txt"
+
+func writeResultToFile(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.1f\n",ebt, profit, ratio)
+	os.WriteFile(resultsFile, []byte(results), 0644)
+
+}
 
 func main() {
 
+	revenue, err := getUserInput("your revenue is ")
 
-	revenue := getUserInput("your revenue is ")
+	if err != nil {
+		
+		fmt.Println(err)
+		
+		// panic("Can't continue, sorry.")
+		return
+	}
 
-	expense := getUserInput("your expense is ")
+	expense, err := getUserInput("your expense is ")
 
-	tax_rate := getUserInput("Tax Rate is ")
+	if err != nil {
+		
+		fmt.Println(err)
+		
+		// panic("Can't continue, sorry.")
+		return
+	}
+
+
+	tax_rate,err := getUserInput("Tax Rate is ")
+
+	if err != nil {
+		
+		fmt.Println(err)
+		
+		// panic("Can't continue, sorry.")
+		return
+	}
+
+	// if err1 != nil || err2 != nil || err3 != nil {
+		
+	// 	fmt.Println(err1)
+		
+	// 	// panic("Can't continue, sorry.")
+	// 	return
+	// }
+
 
 	EBTValue, ProfitValue, RatioValue := calculatorValue(revenue, expense, tax_rate)
 
@@ -20,14 +70,20 @@ func main() {
 	formattedRatio := fmt.Sprintf("Ratio: %.2f\n", RatioValue)
 
 	fmt.Print(formattedEBT, formattedProfit, formattedRatio)
-
+	writeResultToFile(EBTValue, ProfitValue, RatioValue)
 }
 
-func getUserInput(text string) float64 {
+func getUserInput(text string) (float64, error) {
 	var userInput float64
 	fmt.Print(text)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		return 0, errors.New("invalid amount. must be greater than 0")
+
+	}
+
+	return userInput, nil
 }
 
 func calculatorValue(revenue, expense, tax_rate float64) (ebt, profit, ratio float64) {
