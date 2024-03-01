4807a20765e8d422f2934b46011f6325b2b68b30
RadonZ <supachai27102000@gmail.com>
2024-03-01 12:29:29 +0700

    add code working with package part

--- 03-working-with-package/bank/bank.go
+++ 03-working-with-package/bank/bank.go
@@ -1,40 +1,18 @@
 package main
 
 import (
-		"errors"
-		"fmt"
-		"os"
-		"strconv"
-	)
+	"fmt"
+	"examples.com/bank/bank/fileops"
+)
 
 	const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() (float64, error){
-	data, err := os.ReadFile(accountBalanceFile)
 
-	if err != nil {
-	  return 1000, 
-	  errors.New("failed to find balance file")
-	}
-
-	balanceText := string(data)
-	balance,err := strconv.ParseFloat(balanceText, 64)
-
-	if err != nil {
-		return 1000, 
-		errors.New("failed to parse stored balance value")
-	}
-
-	return balance, nil
-}
-
-func writeBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)
-	os.WriteFile(accountBalanceFile,[]byte(balanceText), 0644)
-}
 
 func main() {
-	var accountBalance,err = getBalanceFromFile()
+	var accountBalance,err = fileops.GetBalanceFromFile(accountBalanceFile)
+
+		
 
 	if err != nil {
 		fmt.Println("ERROR")
@@ -46,22 +24,18 @@ func main() {
 	fmt.Println("Welcome to Go Bank!")
 
 	for {
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check balance")
-		fmt.Println("2. Deposit money")
-		fmt.Println("3. Withdraw money")
-		fmt.Println("4. Exit")
+		PresentOptions()
 
 		var choice int
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
 
 		// wantCheckBalance := choice == 1
-		
+
 		switch choice {
 		case 1:
 			fmt.Println("Your balance is", accountBalance)
-			getBalanceFromFile()
+			fileops.GetBalanceFromFile(accountBalanceFile)
 		case 2:
 			fmt.Print("Your deposit: ")
 			var depositAmount float64
@@ -75,7 +49,7 @@ func main() {
 
 			accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
 			fmt.Println("Balance updated! New amount: ", accountBalance)
-			writeBalanceToFile(accountBalance)
+			fileops.WriteBalanceToFile(accountBalance, accountBalanceFile)
 		case 3:
 			fmt.Print("Your withdraw: ")
 			var withdrawalAmount float64
@@ -91,7 +65,7 @@ func main() {
 			}
 			accountBalance -= withdrawalAmount // accountBalance = accountBalance + withdrawalAmount
 			fmt.Println("Balance updated! New amount: ", accountBalance)
-			writeBalanceToFile(accountBalance)
+			fileops.WriteBalanceToFile(accountBalance, accountBalanceFile)
 		default:
 			fmt.Println("Goodbye!")
 			fmt.Println("Thanks for using our service.")
@@ -101,6 +75,5 @@ func main() {
 	}
 }
 
-func presentOptions() {
 
-}
+
