5ac7f62afa6b48a857af25dfb800cdf44c39cee9
RadonZ <supachai27102000@gmail.com>
2024-03-27 18:23:56 +0700

    change directory

--- 07-managing-related-data/lists/lists.go
+++ 07-managing-related-data/lists/lists.go
@@ -1,4 +1,4 @@
-// package lists
+package lists
 
 import "fmt"
 
--- 07-managing-related-data/maps/maps.go
+++ 07-managing-related-data/maps/maps.go
@@ -10,7 +10,10 @@ func main() {
 	fmt.Println(website)
 	fmt.Println(website["Amazon Web Services"])
 	website["LinkedIn"] = "https://linkedin.com"
-	
+
 	delete(website,"Google")
 	fmt.Println(website)
 }
+
+// struct can't delete use for tell type
+// map can remove use for collection
